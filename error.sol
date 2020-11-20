643487262f0d3a711476346efdb82a875f8560db
Джейсон <user@user.com>
2020-11-20 21:14:44 +0300

    Update

---
+++ error.sol
@@ -0,0 +1,32 @@
+pragma solidity >= 0.6.0;
+
+import "main.sol";
+
+contract second {
+    modifier onlyOwnerAndAccept {
+		require(msg.pubkey() == tvm.pubkey());
+		tvm.accept();
+		_;
+	}
+    address adrFirst;
+
+    function change_address(address _adr) public onlyOwnerAndAccept {
+        adrFirst = _adr;
+    }
+
+
+    function setCode(TvmCell newcode) public view onlyOwnerAndAccept {
+		// Runtime function that creates an output action that would change this
+		// smart contract code to that given by cell newcode.
+		tvm.setcode(newcode);
+		// Runtime function that replaces current code of the contract with newcode.
+		tvm.setCurrentCode(newcode);
+	}
+
+    function reverse() public onlyOwnerAndAccept {
+        first(adrFirst).handler(true);
+    }
+	function reverse_send(address _adr) public onlyOwnerAndAccept {
+        first(_adr).handler(true);
+    }
+}
