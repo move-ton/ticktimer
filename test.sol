b1b376a48574acaecd5d830d94c05159dd15b3a1
Джейсон <admin@mcorp.space>
2020-11-23 20:50:48 +0300

    Update test.sol

--- test.sol
+++ test.sol
@@ -1,45 +1,32 @@
 pragma solidity >= 0.6.0;
-pragma AbiHeader expire;
 
-contract ticktimer {
-    function createHandler(uint _payload,uint64 _time) public {}
-    function createTimer(uint _payload,uint64 _time) public {}
-}
+import "main.sol";
 
-contract test {
-    uint count = 0;
-    address callback;
+contract second {
     modifier onlyOwnerAndAccept {
-        require(msg.pubkey() == tvm.pubkey());
-        tvm.accept();
-        _;
+                require(msg.pubkey() == tvm.pubkey());
+                tvm.accept();
+                _;
+        }
+    address adrFirst;
+
+    function change_address(address _adr) public onlyOwnerAndAccept {
+        adrFirst = _adr;
     }
-    modifier alwaysAccept {
-		// Runtime function that allows contract to process inbound messages spending
-		// its own resources (it's necessary if contract should process all inbound messages,
-		// not only those that carry value with them).
-		tvm.accept();
-		_;
-	}
-    function _timer_handler(uint number) public{
-        count = count + number;
-    }
-    function get() public onlyOwnerAndAccept returns(uint) {
-        return count;
-    }
+
 
     function setCode(TvmCell newcode) public view onlyOwnerAndAccept {
-		// Runtime function that creates an output action that would change this
-		// smart contract code to that given by cell newcode.
-		tvm.setcode(newcode);
-		// Runtime function that replaces current code of the contract with newcode.
-		tvm.setCurrentCode(newcode);
-	}
+                // Runtime function that creates an output action that would change this
+                // smart contract code to that given by cell newcode.
+                tvm.setcode(newcode);
+                // Runtime function that replaces current code of the contract with newcode.
+                tvm.setCurrentCode(newcode);
+        }
 
-    function runCallback(uint payload,uint64 timeInt,address timeraddress) public alwaysAccept{
-        ticktimer handler;
-        handler = ticktimer(timeraddress);
-        callback = timeraddress;
-        handler.createHandler(payload,timeInt);
+    function reverse() public onlyOwnerAndAccept {
+        revert(101);
+    }
+        function reverse_send(address _adr) public onlyOwnerAndAccept {
+        first(_adr).handler();
     }
 }
